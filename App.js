2d83313b0d7e4addd930ed0f6add00e540c3ac6c
xofni <fakenewsProject@hotmail.com>
2018-12-02 22:47:00 +0100

    Add NFC

--- App.js
+++ App.js
@@ -1,12 +1,100 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, WebView, AppRegistry } from 'react-native';
+import App2 from './exampleBis/App2'
+import data from './json/news.js';
+import NfcManager, {Ndef} from 'react-native-nfc-manager';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      supported: true,
+      enabled: false,
+      isWriting: false,
+      //urlToWrite: 'https://www.google.com',
+      //rtdType: RtdType.URL,
+      parsedText: null,
+      tag: {},
+      // End NFC
+      news: [],
+      isPlayer : true,
+      showNews : true
+    }
+  }
+
+  componentDidMount(){
+    console.log('Did Mont');
+    NfcManager.isSupported()
+    .then(supported => {
+        this.setState({ supported });
+        if (supported) {
+            this._startNfc();
+        } else {
+          console.log('not supported');
+        }
+    })
+
+   var res = data.news;
+   var indice = getNews();
+   this.setState({ news : res[indice]});
+   this.setState({
+    IdNews: res[0].IdNews,
+    title: res[0].title,
+    shortDesc: res[0].shortDescription,
+    desc: res[0].description,
+    bonus: res[0].bonus,
+    malus: res[0].malus
+  });
+    // Manage NFC
+    //AppRegistry.registerComponent('NfcManagerDev', () => App2);
+
+    function getNews(){
+      return 0;
+    }
+  }
+
+  _startNfc() {
+    NfcManager.start({
+        onSessionClosedIOS: () => {
+            console.log('ios session closed');
+        }
+    })
+        .then(result => {
+            console.log('start OK', result);
+        })
+        .catch(error => {
+            console.warn('start fail', error);
+            this.setState({supported: false});
+        })
+  }
+
   render() {
     return (
+      
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hello !</Text>
+      {this.state.isPlayer === true ?
+        <View>
+        <Text style={styles.text}>{this.state.supported}</Text>
+        <Text style={styles.text}>{this.state.news.shortDescription}</Text>
+        <Text style={styles.text}>{this.state.news.description}</Text>
+        <Text style={styles.text}>{this.state.news.bonus}</Text>
+        <Text style={styles.text}>{this.state.news.malus}</Text>
+        </View>
+        :
+        <Text></Text>
+      }
+      {this.state.showNews === true ?
+        <View>
+        <Text style={styles.text}>{this.state.news.title}</Text>
+        <Text style={styles.text}>{this.state.news.shortDescription}</Text>
+        <Text style={styles.text}>{this.state.news.description}</Text>
+        <Text style={styles.text}>{this.state.news.bonus}</Text>
+        <Text style={styles.text}>{this.state.news.malus}</Text>
+        </View>
+        :
+        <Text></Text>
+      }
+        
       </View>
     );
   }
---
+++ json/news.js
@@ -0,0 +1,86 @@
+const Data = {
+  "news": [
+    {
+      "IdNews": "1",
+      "title":"News 1",
+      "shortDescription":"Short description ",
+      "description": "Description news 1 What When Where Who [Why]",
+      "bonus": "800",
+      "malus": "100"
+    },
+    {
+        "IdNews": "2",
+        "title":"News 2",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "3",
+        "title":"News 3",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "4",
+        "title":"News 4",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "6",
+        "title":"News 6",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "7",
+        "title":"News 7",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "8",
+        "title":"News 8",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "9",
+        "title":"News 9",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "10",
+        "title":"News 10",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      },
+      {
+        "IdNews": "1",
+        "title":"News 1",
+        "shortDescription":"Short description ",
+        "description": "Description news 1 What When Where Who [Why]",
+        "bonus": "800",
+        "malus": "100"
+      }
+]
+};
+export default Data;
+  
